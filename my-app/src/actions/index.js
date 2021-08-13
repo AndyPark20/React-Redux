a5a5bbbf03d1abf08b609e817d2b916311dd151b
Andy Park <apark1214@gmail.com>
2021-08-12 19:53:10 -0700

    in progress

--- my-app/src/actions/index.js
+++ my-app/src/actions/index.js
@@ -1,7 +1,7 @@
 //We are using the same file name called index.js so that when the webpack compiles it, it can search for index.js
 
 //action creator
-const selectSong =(song)=>{
+export const selectSong =(song)=>{
   //return action
   return{
     //We need to have TYPE property as well as an optional payload property for the Action (aka form from the analogy)
@@ -9,3 +9,6 @@ const selectSong =(song)=>{
     payload: song
   };
 };
+
+
+//named export ---> allows us to export many different functions vs. export default can only export one single function
