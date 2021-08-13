8de85c9c3c05d9cd361d89e8d4e3e0fd38852ce8
Andy Park <apark1214@gmail.com>
2021-08-12 19:51:54 -0700

    adding argument song

--- my-app/src/actions/index.js
+++ my-app/src/actions/index.js
@@ -1,10 +1,11 @@
 //We are using the same file name called index.js so that when the webpack compiles it, it can search for index.js
 
 //action creator
-const selectSong =()=>{
+const selectSong =(song)=>{
   //return action
   return{
     //We need to have TYPE property as well as an optional payload property for the Action (aka form from the analogy)
-
+    type:'SONG_SELECTED',
+    payload: song
   };
 };
