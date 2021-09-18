490e1ec8000b69a8cbc2437804793f3c81a66880
Andy Park <apark1214@gmail.com>
2021-09-17 21:08:24 -0700

    completed making reducers

---
+++ my-app/src/reducers/index.js
@@ -0,0 +1,18 @@
+
+const songsReducer =()=>{
+
+  return [
+  {title:'No Scrubs', duration:'4:05'},
+  {title:'Macarena', duration:'2:30'},
+  {title:'All Star', duration:'3:15'},
+  {title:'Martin Garrix', duration:'2:00'}
+  ];
+
+};
+
+const selectedSongReducer=(selectedSong=null, action)=>{
+  if(action.type === 'SONG_SELECTED'){
+    return action.payload
+  }
+  return selectedSong;
+}
