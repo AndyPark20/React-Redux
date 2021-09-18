005a39f0c31d2d97d4638f8b2a426bc4b21ce635
Andy Park <apark1214@gmail.com>
2021-09-18 11:54:55 -0700

    ccreating function to retrieve the object from store?

--- my-app/src/components/app.js
+++ my-app/src/components/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 //if it is a named export then we don't need the curly braces, but if its not, then we do need the curly braces.
-import {SongList} from './songList';
+import SongList from './songList';
 
 
 const App =()=>{
--- my-app/src/components/songList.js
+++ my-app/src/components/songList.js
@@ -1,8 +1,11 @@
 import React,{Component} from 'react';
+import {connect} from 'react-redux';
 
-export class SongList extends Component{
+class SongList extends Component{
   render(){
     return <div>SongList</div>
   }
 
 }
+
+export default connect() (SongList);
