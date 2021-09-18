2cea2726d277e4a6604b3d7192dc106a06c5ed13
Andy Park <apark1214@gmail.com>
2021-09-18 12:06:42 -0700

    got props

--- my-app/src/components/songList.js
+++ my-app/src/components/songList.js
@@ -3,9 +3,19 @@ import {connect} from 'react-redux';
 
 class SongList extends Component{
   render(){
+    console.log(this.props)
     return <div>SongList</div>
   }
 
 }
 
-export default connect() (SongList);
+//We need to get the state object from the "Store" to the Child component here
+//By convention we name the fucntion mapStatetoProps, but can name anything we want
+
+function mapStateToProps(state){
+
+//Will ALWAYS RETURN AN OBJECT --> The component will get it as a props!!!
+return {songs: state.songs};
+}
+
+export default connect(mapStateToProps) (SongList);
