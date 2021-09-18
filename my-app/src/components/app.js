56b18a5f9e4692b6764d029910cb526d0f95dff0
Andy Park <apark1214@gmail.com>
2021-09-17 22:54:27 -0700

    completed songList component with unamed export

--- my-app/src/components/app.js
+++ my-app/src/components/app.js
@@ -1,11 +1,14 @@
 import React from 'react';
 //if it is a named export then we don't need the curly braces, but if its not, then we do need the curly braces.
+import {SongList} from './songList';
 
 
 const App =()=>{
   return(
-    <div>App</div>
-  )
+    <div>
+      <SongList />
+    </div>
+  );
 };
 
 export default App;
---
+++ my-app/src/components/songList.js
@@ -0,0 +1,8 @@
+import React,{Component} from 'react';
+
+export class SongList extends Component{
+  render(){
+    return <div>SongList</div>
+  }
+
+}
