d8c67f815f5ffdadf0fc8b71ec64297e7ed0d223
Andy Park <apark1214@gmail.com>
2021-09-20 20:56:53 -0700

    trying functional components

--- my-app/src/components/songList.js
+++ my-app/src/components/songList.js
@@ -5,27 +5,34 @@ import { connect } from 'react-redux';
 import { selectedSong} from '../actions'
 
 //create a class component
-class SongList extends Component{
+// class SongList extends Component{
 
-  //create a function that will render out the arrays of objects for state of songs
-  renderPlaylist(){
-    return this.props.song.map((values,index)=>{
-      return(
-        <div key={index}>{values.title}
-          <button onClick={() => this.props.selectedSong(values.title)}>Select</button>
-        </div>
-      );
-    });
+//   //create a function that will render out the arrays of objects for state of songs
+//   renderPlaylist(){
+//     return this.props.song.map((values,index)=>{
+//       return(
+//         <div key={index}>{values.title}
+//           <button onClick={() => this.props.selectedSong(values.title)}>Select</button>
+//         </div>
+//       );
+//     });
 
-  };
+//   };
 
 
-  render(){
-    return(
-      <div>{this.renderPlaylist()}</div>
-    );
-  };
-};
+//   render(){
+//     return(
+//       <div>{this.renderPlaylist()}</div>
+//     );
+//   };
+// };
+
+//Create in functional component
+const SongList =(props)=>{
+  return(
+    <div>{console.log(props)}</div>
+  )
+}
 
 //Create a mapStateToProps function that will get the state from the "Store"
 //ONLY FOR STATE
