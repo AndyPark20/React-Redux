bbfd60010902f03a27f2e0a82ccf920a99960f22
Andy Park <apark1214@gmail.com>
2021-10-28 20:03:48 -0700

    created reducer function

--- my-app/src/actions/index.js
+++ my-app/src/actions/index.js
@@ -1,6 +1,6 @@
 // Action Creator (creates Action which is just a plain Js object)
 
-const selectSong =(song)=>{
+export const selectSong =(song)=>{
   //return an action
   return{
     type:'SONG_SELECTED',
---
+++ my-app/src/reducers/index.js
@@ -0,0 +1,15 @@
+const songsReducer = () => {
+  return [
+    { title: "Animals", duration: "4:05" },
+    { title: "I don't wanna miss a thing", duration: "3:15" },
+    { title: "Lucky", duration: "2:45" },
+  ];
+};
+
+
+const selectedSongReducer =(selectedSong =null, action)=>{
+  if(action.type ==='SONG_SELECTED'){
+    return action.payload;
+  };
+  return selectedSong;
+};
