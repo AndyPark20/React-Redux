fb8698b6951a17e3a69916a95c514e2e793d3d71
Andy Park <apark1214@gmail.com>
2021-10-28 19:34:30 -0700

    starting all over with redux songs

--- my-app/src/reducers/index.js
+++ my-app/src/reducers/index.js
@@ -1,29 +1,4 @@
 
-//Import combineReducers method to reduce all the different reducers into one single reducing function
-import { combineReducers } from "redux";
-
-
-//List of Songs (using reducer to render list of songs is an overkill)
-const songReducer = () => {
-  return [
-    { title: 'No Scrubs', duration: '4:05' },
-    { title: 'Macarena', duration: '2:30' },
-    { title: 'All Star', duration: '3:15' },
-    { title: 'Martin Garrix', duration: '2:00' }
-  ];
-};
-
-//Current playing song or change to user Selected song
-const selectedSongReducer =(selectedSong =null,actions)=>{
-    if(actions.type==='SONG_SELECTED'){
-      return actions.payload
-    }
-    return selectedSong;
-}
-
-//use the combinedReducer method to create an object that has all the reducer in "STORE";
-export default combineReducers({
-  song:songReducer,
   selectedSong: selectedSongReducer
 });
 
