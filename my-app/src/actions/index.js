7aadbd557551fe5941af56ae1a658777c51c46a7
Andy Park <apark1214@gmail.com>
2021-11-07 10:13:53 -0800

    going back brushing up redux

--- my-app/src/actions/index.js
+++ my-app/src/actions/index.js
@@ -1,5 +1,6 @@
 // Action Creator (creates Action which is just a plain Js object)
 
+//Since its just a fucntion we do not capitalize the first letter of the function name
 export const selectSong =(song)=>{
   //return an action
   return{
