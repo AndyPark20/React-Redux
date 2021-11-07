736e8a26403754dee8e664fdfcaf6ab78b042612
Andy Park <apark1214@gmail.com>
2021-11-07 14:16:39 -0800

    making action creator

--- my-app/src/components/SongList.js
+++ my-app/src/components/SongList.js
@@ -1,18 +1,34 @@
 import React, {Component} from 'react';
-import {connect} from 'react-redux'
+import {connect} from 'react-redux';
+
+import './songList.css';
 
  class SongList extends Component{
+
+  renderList(){
+    return this.props.songs.map((song,index)=>{
+      return (
+        <div key={index} className="align">
+          <div>{song.title}</div>
+          <button className="button">Select</button>
+        </div>
+      );
+    })
+  }
+
+
   render(){
+    console.log(this.props.songs)
     return(
-      <div>SongList</div>
+      <div>{this.renderList()}</div>
     )
   }
 }
 
 
 const mapStateToProps=(state)=>{
+  return{songs:state.songs};
 
-  return state;
 }
 
 export default connect(mapStateToProps)(SongList);
