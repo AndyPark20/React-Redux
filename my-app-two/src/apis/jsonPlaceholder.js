99b2b03e93c25690d6dff4b7480a9c962884449c
Andy Park <apark1214@gmail.com>
2021-11-09 21:01:45 -0800

    in progress

--- my-app-two/src/apis/jsonPlaceholder.js
+++ my-app-two/src/apis/jsonPlaceholder.js
@@ -4,7 +4,7 @@ export const api = async()=>{
   try{
     const fetchData = await fetch("https://jsonplaceholder.typicode.com/");
     const dataResult = await fetchData.json();
-    console.log(dataResult);
+    return dataResult;
   }catch(err){
     console.error(err)
   }
