4086b2b92870e2f30113fc443f2bd6da988b5b57
Andy Park <apark1214@gmail.com>
2021-11-09 20:45:05 -0800

    working on api setting

---
+++ my-app-two/src/apis/jsonPlaceholder.js
@@ -0,0 +1,11 @@
+
+
+const api = async()=>{
+  try{
+    const fetchData = await fetch("https://jsonplaceholder.typicode.com/");
+    const dataResult = await fetchData.json();
+    console.log(dataResult);
+  }catch(err){
+    console.error(err)
+  }
+}
