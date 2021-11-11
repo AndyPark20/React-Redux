597f236e238f0a72f5d4c4d2abe9d25d72ee4da8
Andy Park <apark1214@gmail.com>
2021-11-10 22:46:16 -0800

    in progress

--- my-app-two/src/actions/index.js
+++ my-app-two/src/actions/index.js
@@ -1,14 +1,6 @@
+import jsonPlaceHolder from '../apis/jsonPlaceholder'
 
-
-export const fetchPosts = ()=>{
-  return async (dispatch)=>{
-     try {
-       const fetchData = await fetch("https://jsonplaceholder.typicode.com/");
-       const dataResult = await fetchData.json();
-       console.log(dataResult);
-     } catch (err) {
-       console.error(err);
-     }
-  }
-
+export const fetchPosts = ()=> async (dispatch)=>{
+  const response = await jsonPlaceHolder.get('/posts');
+  dispatch({type:'FETCH_POSTS', payload:response})
 };
