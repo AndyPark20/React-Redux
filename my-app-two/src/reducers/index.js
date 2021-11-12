6db91182df0c255fe7d58e6ee8772257bdb3f7a8
Andy Park <apark1214@gmail.com>
2021-11-11 20:09:43 -0800

    working on reducer

--- my-app-two/src/reducers/index.js
+++ my-app-two/src/reducers/index.js
@@ -1,12 +1,12 @@
 //No need for importing react because this is just a function
 
 import { combineReducers } from "redux";
-
+import postReducer from "./postReducer";
 
 
 
 
 export default combineReducers({
   //Put dummy variable
-  replaceMe:()=>10
+  posts:postReducer
 });
--- my-app-two/src/reducers/postReducer.js
+++ my-app-two/src/reducers/postReducer.js
@@ -1,3 +1,6 @@
+
+
+
 export default()=>{
   return 123;
 }
