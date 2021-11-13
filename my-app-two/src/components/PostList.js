7d16ad3a7f88ab88016ebc0a31051949e89b501d
Andy Park <apark1214@gmail.com>
2021-11-13 10:36:34 -0800

    adding css style

--- my-app-two/src/components/PostList.js
+++ my-app-two/src/components/PostList.js
@@ -2,20 +2,43 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {fetchPosts} from '../actions'
 
+
+import from ''
+
 class PostList extends React.Component{
 
   componentDidMount(){
     this.props.fetchPosts();
   }
 
-  render(){
-    console.log(this.props.posts)
-    return <div>Post List</div>
+  renderList(){
+    const returndata= this.props.posts.map(values=>{
+      return (
+        <div className="card-container">
+          <div className="row">
+            <div className="col">
+              <i class="far fa-id-card"></i>
+              <div>
+                <h2>{values.title}</h2>
+                <p>{values.body}</p>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      );
+    })
+    return returndata;
   }
-}
+
+
+  render(){
+    return <div>{this.renderList()}</div>
+  };
+};
 
 const mapStateToprops =(state)=>{
-  return{posts:state.posts}
+  return{posts:state.posts};
 }
 
-export default connect(mapStateToprops,{fetchPosts})(PostList)
+export default connect(mapStateToprops,{fetchPosts})(PostList);
