3e9ec106f41a171df7dcabbaeca8e433ab74d065
Andy Park <apark1214@gmail.com>
2021-11-13 10:42:53 -0800

    completed

--- my-app-two/src/components/PostList.js
+++ my-app-two/src/components/PostList.js
@@ -1,44 +1,40 @@
-import React from 'react';
-import {connect} from 'react-redux';
-import {fetchPosts} from '../actions'
+import React from "react";
+import { connect } from "react-redux";
+import { fetchPosts } from "../actions";
 
+import "./component.css";
 
-import from ''
-
-class PostList extends React.Component{
-
-  componentDidMount(){
+class PostList extends React.Component {
+  componentDidMount() {
     this.props.fetchPosts();
   }
 
-  renderList(){
-    const returndata= this.props.posts.map(values=>{
+  renderList() {
+    const returndata = this.props.posts.map((values, index) => {
       return (
-        <div className="card-container">
+        <div className="card-container" key={index}>
           <div className="row">
-            <div className="col">
-              <i class="far fa-id-card"></i>
-              <div>
+            <div className="col details">
+              <i className="far fa-id-card"></i>
+              <div className="body">
                 <h2>{values.title}</h2>
                 <p>{values.body}</p>
               </div>
-
             </div>
           </div>
         </div>
       );
-    })
+    });
     return returndata;
   }
 
-
-  render(){
-    return <div>{this.renderList()}</div>
-  };
-};
-
-const mapStateToprops =(state)=>{
-  return{posts:state.posts};
+  render() {
+    return <div>{this.renderList()}</div>;
+  }
 }
 
-export default connect(mapStateToprops,{fetchPosts})(PostList);
+const mapStateToprops = (state) => {
+  return { posts: state.posts };
+};
+
+export default connect(mapStateToprops, { fetchPosts })(PostList);
