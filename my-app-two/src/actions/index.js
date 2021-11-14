fea8c3661f50004000504872adb7e5d069344517
Andy Park <apark1214@gmail.com>
2021-11-13 18:03:19 -0800

    using alternative solution for meoize

--- my-app-two/src/actions/index.js
+++ my-app-two/src/actions/index.js
@@ -9,11 +9,21 @@ export const fetchPosts = ()=> async (dispatch)=>{
 };
 
 
-export const fetchUser =id=>dispatch=>_fetchUSer(id,dispatch);
-
-
+export const fetchUser =id=>async dispatch=>{
 //You can only fetch this one time!
-const _fetchUSer = _.memoize(async (id, dispatch)=>{
     const response = await jsonPlaceHolder.get(`/users/${id}`);
     dispatch({ type: "FETCH_USER", payload: response.data });
-});
+};
+
+
+
+
+
+
+
+
+// //You can only fetch this one time!
+// const _fetchUSer = _.memoize(async (id, dispatch)=>{
+//     const response = await jsonPlaceHolder.get(`/users/${id}`);
+//     dispatch({ type: "FETCH_USER", payload: response.data });
+// });
