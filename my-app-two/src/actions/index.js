1fada3b04a20bf19db16e9bac298a94db02d7d68
Andy Park <apark1214@gmail.com>
2021-11-13 18:28:00 -0800

    fixed self fetcching issue

--- my-app-two/src/actions/index.js
+++ my-app-two/src/actions/index.js
@@ -4,6 +4,7 @@ import _ from 'lodash';
 
 export const fetchPostsAndUsers =()=> async (dispatch,getState)=>{
    await dispatch(fetchPosts());
+   console.log(getState().posts)
    const userIds=_.uniq(_.map(getState().posts, 'userId'));
    userIds.forEach(id=>dispatch(fetchUser(id)));
 }
