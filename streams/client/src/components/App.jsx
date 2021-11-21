79d6d8106ae4247965114c9e9d6da82dc533fb07
Andy Park <apark1214@gmail.com>
2021-11-21 09:28:05 -0800

    working how to route navigation

--- streams/client/src/components/App.jsx
+++ streams/client/src/components/App.jsx
@@ -1,32 +1,39 @@
-import React from 'react';
-import { BrowserRouter, Route  } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 
-const pageOne =()=>{
-  return(
-    <div>PageOne</div>
+const pageOne = () => {
+  return (
+    <div>
+      <h2>Page One!</h2>
+      <Link to="pageTwo">Navigate to Page Two</Link>
+    </div>
+
+
     //BAD PRACTICE!!!
     //<a href="/pageTwo">Navigate to Page Two</a>
-      //=== Will Fetch new HTML doc and will lose all your JS file!!!
-      //=== Including all the inputs that user has submitted and even all the data that was fetched from the API!
-  )
-}
+    //=== Will Fetch new HTML doc and will lose all your JS file!!!
+    //=== Including all the inputs that user has submitted and even all the data that was fetched from the API!
+  );
+};
 
-const pageTwo =()=>{
-  return(
-    <div>PageTwo</div>
-  )
-}
+const pageTwo = () => {
+  return (
+    <div>
+      <h2>Page Two!</h2>
+      <Link to="/">Navigate to Page One</Link>
+    </div>
+  );
+};
 
-const App =()=>{
-  return(
+const App = () => {
+  return (
     <BrowserRouter>
       <div>
         <Route path="/" exact component={pageOne}></Route>
         <Route path="/pageTwo" component={pageTwo}></Route>
       </div>
     </BrowserRouter>
-  )
+  );
 };
-
 
 export default App;
