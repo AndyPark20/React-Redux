4b0df04883f937450598ec5f9143fcb00d3fb1aa
Andy Park <apark1214@gmail.com>
2021-11-22 20:48:36 -0800

    completed setting up component in App file

--- streams/client/src/components/App.jsx
+++ streams/client/src/components/App.jsx
@@ -1,38 +1,27 @@
 import React from "react";
 import { BrowserRouter, Route, Link } from "react-router-dom";
-
-const pageOne = () => {
-  return (
-    <div>
-      <h2>Page One!</h2>
-      <Link to="pageTwo">Navigate to Page Two</Link>
-    </div>
+import StreamCreate from './streams/streamCreate';
+import StreamDelete from "./streams/streamDelete";
+import StreamEdit from "./streams/streamEdit";
+import StreamList from "./streams/streamList";
+import StreamShow from "./streams/streamShow";
 
 
-    //BAD PRACTICE!!!
-    //<a href="/pageTwo">Navigate to Page Two</a>
-    //=== Will Fetch new HTML doc and will lose all your JS file!!!
-    //=== Including all the inputs that user has submitted and even all the data that was fetched from the API!
-  );
-};
 
-const pageTwo = () => {
-  return (
-    <div>
-      <h2>Page Two!</h2>
-      <Link to="/">Navigate to Page One</Link>
-    </div>
-  );
-};
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <div>
-        <Route path="/" exact component={pageOne}></Route>
-        <Route path="/pageTwo" component={pageTwo}></Route>
-      </div>
-    </BrowserRouter>
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path='/' exact component={StreamList} />
+          <Route path = '/streams/new' exact component ={StreamCreate}/>
+          <Route path='/streams/edit' exact component={StreamEdit}/>
+          <Route path='/streams/delete' exact component ={StreamDelete}/>
+          <Route path='/streams/show' exact  component ={StreamShow}/>
+        </div>
+      </BrowserRouter>
+    </div>
   );
 };
 
