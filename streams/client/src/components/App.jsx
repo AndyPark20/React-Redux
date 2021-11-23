c444574acc05c2710940a614fda4b46eda2d11c8
Andy Park <apark1214@gmail.com>
2021-11-22 21:13:04 -0800

    completed routing

--- streams/client/src/components/App.jsx
+++ streams/client/src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter, Route} from "react-router-dom";
 import StreamCreate from './streams/streamCreate';
 import StreamDelete from "./streams/streamDelete";
 import StreamEdit from "./streams/streamEdit";
@@ -13,14 +13,15 @@ import Header from "./Header";
 const App = () => {
   return (
     <div className="ui container">
-      <Header/>
       <BrowserRouter>
         <div>
-          <Route path='/' exact component={StreamList} />
-          <Route path = '/streams/new' exact component ={StreamCreate}/>
-          <Route path='/streams/edit' exact component={StreamEdit}/>
-          <Route path='/streams/delete' exact component ={StreamDelete}/>
-          <Route path='/streams/show' exact  component ={StreamShow}/>
+          <Header />
+
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
         </div>
       </BrowserRouter>
     </div>
