d3bf65ed5de10a21a727181af02647a70a6bb17f
Andy Park <apark1214@gmail.com>
2021-11-27 09:24:29 -0800

    working on gapi functions

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -2,6 +2,17 @@ import React from 'react';
 
 
 class GoogleAuth extends React.Component{
+
+  componentDidMount(){
+    window.gapi.load('client:auth2',()=>{
+      window.gapi.client.init({
+        clientId:
+          "115216850645-dvhh369cv2ht2opv7lq30aif2f7r0ru9.apps.googleusercontent.com",
+      });
+    });
+
+  }
+
   render(){
     return <div>Google Auth</div>
   }
