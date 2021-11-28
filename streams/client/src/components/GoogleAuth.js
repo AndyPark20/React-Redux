3a1774cdae0e73f974d90c3f9f11e58164ca525e
Andy Park <apark1214@gmail.com>
2021-11-28 13:26:19 -0800

    email oAuth scrope

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -7,7 +7,8 @@ class GoogleAuth extends React.Component{
     window.gapi.load('client:auth2',()=>{
       window.gapi.client.init({
         clientId:
-          "115216850645-dvhh369cv2ht2opv7lq30aif2f7r0ru9.apps.googleusercontent.com",
+          '115216850645-dvhh369cv2ht2opv7lq30aif2f7r0ru9.apps.googleusercontent.com',
+          scope:'email'
       });
     });
 
