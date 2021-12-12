a7dcb9ac340b99a264c2badb70efdf9fab05c9e0
Andy Park <apark1214@gmail.com>
2021-12-11 16:10:20 -0800

    working on case switched

--- streams/client/src/reducers/index.js
+++ streams/client/src/reducers/index.js
@@ -2,9 +2,16 @@ import { combineReducers } from "redux";
 
 
 const signedInOrOutFunction =(status =null, action)=>{
-  console.log(action)
-  if(action.type ==='SIGN_OUT'){
-    return action.payload;
+  console.log(action.type)
+  // if(action.type ==='SIGN_OUT'){
+  //   return action.payload;
+  // }
+  // return status;
+  switch(action.type){
+    case 'SIGN_OUT':
+      console.log('signedOUT!');
+      default:
+        console.log('nothing found!')
   }
   return status;
 }
